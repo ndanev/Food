9889ddc505c85ca5e20a538c308a474be62f8128
Nemanja Danev <nemanja.danev.93@gmail.com>
2020-04-07 11:12:23 +0200

    added login and register routes

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,17 @@
 module.exports = {
     index: (req, res) => {
         res.render('default/index');
+    },
+    loginGet: (req, res) => {
+        res.render('default/login');
+    },
+    loginPost: (req, res) => {
+        res.send('Congrass you are logged in!')
+    },
+    registerGet: (req, res) => {
+        res.render('default/register');
+    },
+    registerPost: (req, res) => {
+        res.send('Congrass you are registerd!');
     }
 }
--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -4,4 +4,10 @@ const defaultController = require('../controllers/defaultController');
 
 router.get('/', defaultController.index);
 
+router.get('/login', defaultController.loginGet);
+router.post('/login', defaultController.loginPost);
+
+router.get('/register', defaultController.registerGet);
+router.post('/register', defaultController.registerPost);
+
 module.exports = router;
